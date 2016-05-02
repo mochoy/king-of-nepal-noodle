ba44bd0dbb9a34877a68e2cd508d94457a3c86b0
Monte <BobLakensi@users.noreply.github.com>
2016-05-01 19:21:17 -0700

    Fix tower1 image, create drawRange function

--- TD-Game/JS/towers.js
+++ TD-Game/JS/towers.js
@@ -1,11 +1,19 @@
 var towerStuff = {};
 
+towerStuff.allTowerArray = new Array();
+towerStuff.tower1Array = new Array();
+
 towerStuff.Tower1 = function () {
-    this.range = 10;
+    this.range = 100;
     this.damage = 10;
     this.fireRate = 10,
     this.image = 'tower1IMG';
     
+    this.sprite = null;
+    
+    this.drawRange = function (){
+        towerStuff.drawRange(this)
+    }
 }
 
 towerStuff.buyTower = function (towerToBuy, towerSlot) {
@@ -24,9 +32,24 @@ towerStuff.buyTower = function (towerToBuy, towerSlot) {
 
 towerStuff.placeTower = function (tower, x, y) {
     //create new instance of tower class
-    new tower;
+    var tower = new towerStuff.Tower1();
     
     //add a sprite to the new tower object
     tower.sprite = game.add.sprite(x, y, tower.image);
     game.physics.arcade.enable(tower.sprite);
+    
+    tower.drawRange();
+}
+
+towerStuff.drawRange = function (towerObject) {
+    var sprite = towerObject.sprite;
+    
+    //get coords to draw range
+    var xCoords = sprite.body.x;
+    var yCoords = sprite.body.y;
+    
+    //get range to draw circle
+    var d = towerObject.range;
+    
+    console.log("drawing range")
 }
