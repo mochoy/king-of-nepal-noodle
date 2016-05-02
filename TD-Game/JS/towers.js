d2631a5bb9dae94ab3c5ef33c3d10cd19d64d8c8
Monte <BobLakensi@users.noreply.github.com>
2016-05-01 19:10:59 -0700

    Tower slot killed when new tower bought

--- TD-Game/JS/towers.js
+++ TD-Game/JS/towers.js
@@ -9,17 +9,24 @@ towerStuff.Tower1 = function () {
 }
 
 towerStuff.buyTower = function (towerToBuy, towerSlot) {
+    //get coords for where to spawn tower
     var xCoord = towerSlot.body.x;
     var yCoord = towerSlot.body.y;
     
+    //check which tower to buy
     if (towerToBuy == 1){
         towerStuff.placeTower(towerStuff.Tower1, xCoord, yCoord);
     }
+    
+    //remove tower slot
+    towerSlot.kill();
 }
 
 towerStuff.placeTower = function (tower, x, y) {
+    //create new instance of tower class
     new tower;
     
+    //add a sprite to the new tower object
     tower.sprite = game.add.sprite(x, y, tower.image);
     game.physics.arcade.enable(tower.sprite);
 }
