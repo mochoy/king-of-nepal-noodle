9629ae76829d1faab99363ea14fecf1462ff3575
Monte <montgomery.okdaily@gmail.com>
2016-06-21 03:58:14 +0000

    Check if enemy reaches home

--- TD-Game/JS/mainGame.js
+++ TD-Game/JS/mainGame.js
@@ -78,6 +78,12 @@ var mainGameVar = {
             towerStuff.towerNeedSearchArray[tower].findEnemy(enemyStuff.allEnemyArray);
         }   //for tower find enemy
         
+        //when enemy reaches end
+        for (var enemy = 0; enemy < enemyStuff.allEnemyArray.length; enemy++){
+            var enemySprite = enemyStuff.allEnemyArray[enemy];
+            game.physics.arcade.overlap(enemySprite, enemyStuff.moveToPoint, enemySprite.homeReached, null, this);
+        }   //for 
+        
     }  //function update
     
 }   //object mainGameVar
