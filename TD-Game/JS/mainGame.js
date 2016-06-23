fdbde088fdb8cb2f3a8a25c48a7dd9b697148168
Monte <montgomery.okdaily@gmail.com>
2016-06-23 00:18:52 +0000

    Basic tower created when game starts

--- TD-Game/JS/mainGame.js
+++ TD-Game/JS/mainGame.js
@@ -36,7 +36,7 @@ var mainGameVar = {
 
 
         towerStuff.createTower(0, game.world.width/2, 500);
-
+        towerStuff.createTower(1, 100, 100);
         
         new towerStuff.TowerSlotPrototype().create(game, 500, 500);
 
