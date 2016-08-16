37b8a070b308a0007a07ecfbaee49b8e80d56da8
BobLakensi <montgomery.okdaily@gmail.com>
2016-08-15 20:25:46 -0700

    added vars to keep track of upgrades in UpgradeManager

--- TD-Game/JS/UpgradeManager.js
+++ TD-Game/JS/UpgradeManager.js
@@ -1,3 +1,11 @@
-var UpgradeManager = function () {
+var UpgradeManager = function (upgradeData) {
+    //keep track of upgrades on each path
+    this.currentUps1 = 1;
+    this.currentUps2 = 1;
+    this.currentUps3 = 1;
+    
+    //whatever's being upgraded gets a new copy of the JSON object containing all of the data 
+    this.data = Object.create(upgradeData);
+
     
 }
