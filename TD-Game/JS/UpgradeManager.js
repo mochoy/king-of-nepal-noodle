51d8b9d093a7b6f2b2dee06f91ef79b5acbe3c59
BobLakensi <montgomery.okdaily@gmail.com>
2016-08-15 20:36:35 -0700

    currrentUps var starts at 0 to parallel array with subscript

--- TD-Game/JS/UpgradeManager.js
+++ TD-Game/JS/UpgradeManager.js
@@ -1,17 +1,17 @@
 var UpgradeManager = function (upgradeData) {
     //keep track of upgrades on each path
-    this.currentUps1 = 1;
-    this.currentUps2 = 1;
-    this.currentUps3 = 1;
+    this.currentUps1 = 0;
+    this.currentUps2 = 0;
+    this.currentUps3 = 0;
     
     //Entity being upgraded gets a new copy of the JSON object containing all of the data 
     this.data = Object.create(upgradeData);
 
     this.validateUpgradeEntity = function (path) {        
         //money and upgrades validation before upgrade
-        if (data.money < upgradeData.[path].cost;) {
+        if (data.money < upgradeData.path.cost;) {
             //too poor, can't buy
-        } else if (data.money >= upgradeData.[path].cost;) {
+        } else if (data.money >= upgradeData.path.cost;) {
             this.upgradeEntity();
         }
         
