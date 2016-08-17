db0957c773f43662dedac0638d0e24222b3239e3
BobLakensi <montgomery.okdaily@gmail.com>
2016-08-16 20:28:56 -0700

    added variables to keep track of upgrades in paths in data file

--- TD-Game/JS/UpgradeManager.js
+++ TD-Game/JS/UpgradeManager.js
@@ -1,28 +1,27 @@
 var UpgradeManager = function (upgradeData) {
-    //keep track of upgrades on each path
-    this.currentUps1 = 0;
-    this.currentUps2 = 0;
-    this.currentUps3 = 0;
-    
     //Entity being upgraded gets a new copy of the JSON object containing all of the data 
     this.data = Object.create(upgradeData);
 
-    this.validateUpgradeEntity = function (path) {        
+    //make sure entitiy can be upgraded 
+    //path will be a number correspoding to the value of currentPathUps      
+    this.validateUpgradeEntity = function (path) {      
         //money and upgrades validation before upgrade
         if (data.money < upgradeData.path.cost) {
             //too poor, can't buy
         } else if (data.money >= upgradeData.path.cost) {
-            this.upgradeEntity();
+            this.upgradeEntity(path);
         }
         
         
     }    //method
     
-    this.upgradeEntity = function () {
-        window["currentUps" + path]++;
+    this.upgradeEntity = function (path) {
+        upgradeData.window["currentUpsPath" + path]++;      //reference to variable keeping track of upgrades on each path
+        //window["currentUps" + path]++;
         data.money -= upgradeData[path].cost;
         UI.updatateUI();
-    }
+        
+    }   //method
 
 
 }   //constructor
