d8e0761e1724f0d2ee74f3177affed50d6d345d1
BobLakensi <montgomery.okdaily@gmail.com>
2016-08-16 20:32:52 -0700

    fixed conditions of validateUpgradeEnitity(), created displayUpgradeInfo()

--- TD-Game/JS/UpgradeManager.js
+++ TD-Game/JS/UpgradeManager.js
@@ -1,14 +1,21 @@
-var UpgradeManager = function (upgradeData) {
+var UpgradeManager = function (tower, upgradeData) {
     //Entity being upgraded gets a new copy of the JSON object containing all of the data 
     this.data = Object.create(upgradeData);
+    this.tower = tower;
+    
+    //when entity clicked, display upgrade information
+    this.displayUpgradeInfo = function (path) {
+        //temporary code before disigning actual stuff
+        this.validatedUpgradeEntity(path);
+    }
 
     //make sure entitiy can be upgraded 
     //path will be a number correspoding to the value of currentPathUps      
     this.validateUpgradeEntity = function (path) {      
         //money and upgrades validation before upgrade
-        if (data.money < upgradeData.path.cost) {
+        if (data.money < upgradeData.window["currentUpsPath" + path].cost) {
             //too poor, can't buy
-        } else if (data.money >= upgradeData.path.cost) {
+        } else if (data.money >= upgradeData.window["currentUpsPath" + path].cost) {
             this.upgradeEntity(path);
         }
         
