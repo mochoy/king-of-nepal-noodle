c2c94ac9ac8e7a7e9ca4f8bb05aa7598e258055f
BobLakensi <montgomery.okdaily@gmail.com>
2016-09-08 19:00:59 -0700

    trapFactory creates and returns new Trap

--- TD-Game/JS/Traps.js
+++ TD-Game/JS/Traps.js
@@ -1,14 +1,18 @@
-var TrapPrototype = function (game, x, y, data){
+var trapStuff = {
+    allTrapArr: []
+};
+
+var TrapPrototype = function (game, x, y, data){    
     this.inheritEntity = function (thiz, constructer) {
         thiz.constructer = constructer;
-        thiz.constructer(x, y, data, src);
+        thiz.constructer(x, y, data, data.src);
     };
-    this.inheritBuildingsAndTowers(this, Entity);
+    this.inheritEntity(this, Entity);
 }
 
 
 
 
-var trapFactory = function (towerNum, x, y) {
-    return new TrapPrototype();
+trapStuff.trapFactory = function (trapNum, x, y) {
+    return new window[trapData.data[trapNum].class](game, x, y, trapData.data[trapNum]).addToArr(null); 
 }
