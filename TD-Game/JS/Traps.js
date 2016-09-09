8515afb2b03f97c88a96993c7c15e5aac9e67ad2
BobLakensi <montgomery.okdaily@gmail.com>
2016-09-08 20:11:38 -0700

    create and call addToArr method of Trap class

--- TD-Game/JS/Traps.js
+++ TD-Game/JS/Traps.js
@@ -2,17 +2,28 @@ var trapStuff = {
     allTrapArr: []
 };
 
-var TrapPrototype = function (game, x, y, data){    
+var TrapPrototype = function (x, y, data){        
     this.inheritEntity = function (thiz, constructer) {
         thiz.constructer = constructer;
         thiz.constructer(x, y, data, data.src);
     };
     this.inheritEntity(this, Entity);
-}
+    
+    this.addToArr = function (index) {              
+        if (!index) {
+            trapStuff.allTrapArr.push(this.sprite);
+        }
+        
+        return this;
+    }
+    
+    return this;
+    
+}   // constructor
 
 
 
 
 trapStuff.trapFactory = function (trapNum, x, y) {
-    return new window[trapData.data[trapNum].class](game, x, y, trapData.data[trapNum]).addToArr(null); 
+    return new window[trapData.data[trapNum].class](x, y, trapData.data[trapNum]).addToArr(null); 
 }
