c81b8784a41274e4299d658d4c3bdb13be6b0932
BobLakensi <montgomery.okdaily@gmail.com>
2016-09-08 18:02:35 -0700

    create TrapProtype constuctor inheriting from Entity

--- TD-Game/JS/Traps.js
+++ TD-Game/JS/Traps.js
@@ -1,2 +1,7 @@
-//first commit
-console.log("traps!");
+var TrapPrototype = function (game, x, y, data){
+    this.inheritEntity = function (thiz, constructer) {
+        thiz.constructer = constructer;
+        thiz.constructer(x, y, data, src);
+    };
+    this.inheritBuildingsAndTowers(this, Entity);
+}
