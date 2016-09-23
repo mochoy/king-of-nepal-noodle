d7d7cf0bdebecc297629375a751ab816761783b5
BobLakensi <montgomery.okdaily@gmail.com>
2016-09-22 18:01:18 -0700

    create helper method to check if overlapping right

--- TD-Game/JS/helper.js
+++ TD-Game/JS/helper.js
@@ -165,9 +165,23 @@ helper.isOverlappingLeft = function (sprite1, sprite2) {
     if ( (sprite1.y > sprite2.y - sprite2.height/2) 
         && (sprite1.y < sprite2.y + sprite2.height/2)
         && (sprite1.x < sprite2.x - sprite2.width/2) ) {
+        
         return true;
     }
-     
+    
+    return false;
+    
+    
+}
+
+helper.isOverlappingRight = function (sprite1, sprite2) {
+    if ( (sprite1.y > sprite2.y - sprite2.height/2)
+        && (sprite1.y < sprite2.y + sprite2.height/2) 
+        && (sprite1.x > sprite2.x - sprite2.width/2) ) {
+        
+        return true;
+    }
+    
     return false;
     
     
